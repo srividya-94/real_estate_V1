8c1625a2c7c32562d5d646cbaa3fbc9e6a355437
Srividya Loganadane <slogana@eu.trimblecorp.net>
2025-01-30 15:41:56 +0530

    address_console.sql

--- models/L3_CONSUMPTION/Address_console.sql
+++ models/L3_CONSUMPTION/Address_console.sql
@@ -1,7 +1,3 @@
--- with address_console AS(
--- select a.* , b.* from {{( 'L1_LANDING.point_of_interests_addresses_relationships' )}} a 
--- join {{( 'L1_LANDING.US_ADDRESSES' )}} b on a.ADDRESS_ID = b.ADDRESS_ID)
--- select * from address_console
 
 with address_console AS(
 select b.* , a.poi_id,a.RELATIONSHIP_TYPE from US_REAL_ESTATE.CYBERSYN.POINT_OF_INTEREST_ADDRESSES_RELATIONSHIPS  a 
